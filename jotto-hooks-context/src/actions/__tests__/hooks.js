c723f9d9039511ef9ea75863fd78e5196d2c886f
weehaa <vzg1000@mail.ru>
2020-08-24 17:20:39 +0400

    add getSecretWord hook action and tests with moxios

---
+++ jotto-hooks-context/src/actions/__tests__/hooks.js
@@ -0,0 +1,31 @@
+import moxios from 'moxios';
+
+import { getSecretWord } from '../hooks';
+
+describe('mpxios tests', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test('calls the getSecretWord callback on axios response', async () => {
+    const secretWord = 'party';
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+    //create mock for callback arg
+    const mockSetSecretWord = jest.fn();
+    await getSecretWord(mockSetSecretWord);
+    // check whether mock was run with the correct argument
+
+    expect(mockSetSecretWord).toHaveBeenCalledWith(secretWord);
+
+  });
+});
