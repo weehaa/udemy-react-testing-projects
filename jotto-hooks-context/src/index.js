c8eae64e73753a6f7b200a94fda49ea785f24968
weehaa <vzg1000@mail.ru>
2020-09-15 07:35:03 +0400

    add LanguageProvider to index.js

--- jotto-hooks-context/src/index.js
+++ jotto-hooks-context/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
+import { LanguageProvider} from './contexts/language-context';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </Router>,
   document.getElementById('root')
 );
